0090cb85a7a35dd8ac058b6116c325ce0e484eac
Pradeep Kumar <kumarpradeep@vmware.com>
2020-01-03 09:14:00 +0530

    User Controller End point - Initial commit
    initial commit for sign up end point

---
+++ quora-service/src/main/java/com/upgrad/quora/service/com/upgrad/quora/service/dao/UserDao.java
@@ -0,0 +1,21 @@
+package com.upgrad.quora.service.com.upgrad.quora.service.dao;
+
+import com.upgrad.quora.service.entity.UserEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class UserDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public UserEntity createUser(UserEntity userEntity) {
+        entityManager.persist(userEntity);
+        return userEntity;
+
+    }
+}
