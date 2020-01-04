76b7d3398e1064b107cec097ac8876a37ebb35a7
randheerr <randheerr@vmware.com>
2020-01-05 02:27:21 +0530

    Implementing the endpoint to retrieve user details from database
    - Details fetched using userId
    - Appropriate error responses to be returned if access token is not present in DB or user has signed out and user does not exist in DB

---
+++ quora-service/src/main/java/com/upgrad/quora/service/dao/UserDao.java
@@ -0,0 +1,30 @@
+package com.upgrad.quora.service.dao;
+
+import com.upgrad.quora.service.entity.UserEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class UserDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    /**
+     * Fetch a single user by given id from the database.
+     *
+     * @param userId Id of the user whose information is to be retrieved.
+     * @return User details if exist in the DB else null.
+     */
+    public UserEntity getUserById(final String userId) {
+        try {
+            return entityManager.createNamedQuery("userByUserId", UserEntity.class).setParameter("userId", userId).getSingleResult();
+        } catch (NoResultException nre) {
+            return null;
+        }
+    }
+}
